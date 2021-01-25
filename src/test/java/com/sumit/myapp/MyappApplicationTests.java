a5c598f732bca7c0e0e1e610e56df72183c67115
Sumit Sharma <sumit.s@abc.com>
2021-01-25 15:52:14 +0530

    First Commit

---
+++ src/test/java/com/sumit/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumit.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
